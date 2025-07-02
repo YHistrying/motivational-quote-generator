08615b6f6b7da8effa9c297e0a4b9075eb8c7960
yijenislearningtocode <yijenhs@gmail.com>
2025-07-02 19:34:15 +0200

    Added JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote-text", {
+    strings: ["Generating your quote..."],
+    autoStart: true,
+    loop: false,
+    cursor: "",
+    delay: 75,
+  });
+}
+
+let topicFrom = document.querySelector("#quote-topic");
+topicFrom.addEventListener("submit", generateQuote);
